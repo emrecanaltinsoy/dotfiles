4367ce8257b9f8856daff9fc42a23f7cd7aec637
Emrecan Altinsoy <emrecanaltinsoy@gmail.com>
2025-11-26 18:01:57 +0100

    Update nvim opencode config

--- nvim/.config/nvim/lua/plugins/opencode.lua
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -1,68 +1,40 @@
 return {
   {
-    "nickvandyke/opencode.nvim",
+    "NickvanDyke/opencode.nvim",
     dependencies = { "folke/snacks.nvim" },
+    config = function()
+      -- Required for `opts.events.reload`.
+      vim.o.autoread = true
+
+      vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+        require("opencode").ask("@this: ", { submit = true })
+      end, { desc = "Ask opencode" })
+
+      vim.keymap.set({ "n", "x" }, "<leader>ox", function()
+        require("opencode").select()
+      end, { desc = "Execute opencode action…" })
+
+      vim.keymap.set({ "n", "x" }, "<leader>op", function()
+        require("opencode").prompt("@this")
+      end, { desc = "Add to opencode" })
+
+      vim.keymap.set({ "n", "x" }, "<leader>op", function()
+        require("opencode").prompt("@this")
+      end, { desc = "Add to opencode" })
+
+      vim.keymap.set("n", "<leader>on", function()
+        require("opencode").command("session.new")
+      end, { desc = "New session" })
+
+      vim.keymap.set("n", "<leader>oi", function()
+        require("opencode").command("session.interrupt")
+      end, { desc = "New session" })
+
+      vim.keymap.set({ "n", "t" }, "<leader>ot", function()
+        require("opencode").toggle()
+      end, { desc = "Toggle opencode" })
+    end,
+
     opts = {},
-    keys = {
-      {
-        "<leader>ot",
-        function()
-          require("opencode").toggle()
-        end,
-        desc = "toggle embedded opencode",
-      },
-      {
-        "<leader>oa",
-        function()
-          require("opencode").ask()
-        end,
-        desc = "ask opencode",
-        mode = "n",
-      },
-      {
-        "<leader>oa",
-        function()
-          require("opencode").ask("@selection: ")
-        end,
-        desc = "ask opencode about selection",
-        mode = "v",
-      },
-      {
-        "<leader>op",
-        function()
-          require("opencode").select_prompt()
-        end,
-        desc = "select prompt",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>on",
-        function()
-          require("opencode").command("session_new")
-        end,
-        desc = "new session",
-      },
-      {
-        "<leader>oy",
-        function()
-          require("opencode").command("messages_copy")
-        end,
-        desc = "copy last message",
-      },
-      {
-        "<s-c-u>",
-        function()
-          require("opencode").command("messages_half_page_up")
-        end,
-        desc = "scroll messages up",
-      },
-      {
-        "<s-c-d>",
-        function()
-          require("opencode").command("messages_half_page_down")
-        end,
-        desc = "scroll messages down",
-      },
-    },
   },
 }
