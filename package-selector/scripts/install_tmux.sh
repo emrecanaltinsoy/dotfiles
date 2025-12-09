f767ce10c8fa598e245aa2dbe74a3af5c65e67e9
Emrecan Altinsoy <emrecan.altinsoy@se.com>
2025-12-09 15:02:53 +0100

    Update tmux config

--- package-selector/scripts/install_tmux.sh
+++ package-selector/scripts/install_tmux.sh
@@ -4,6 +4,8 @@ shopt -s expand_aliases
 
 source "$HOME/.config/zsh/shared-aliases"
 
+OMT_PATH="$HOME/oh-my-tmux"
+
 if [ ! -x "$(command -v tmux)" ]; then
   echo "tmux is not installed! installing now."
   install -y tmux || exit 1
@@ -11,16 +13,17 @@ else
   echo "tmux is already installed."
 fi
 
-if [[ ! -d ~/.tmux ]]; then
-  git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-  ln -s -f .tmux/.tmux.conf
-  cp .tmux/.tmux.conf.local .
+if [[ ! -d $OMT_PATH ]]; then
+  cd "$HOME" || exit 1
+  git clone --single-branch https://github.com/gpakosz/.tmux.git "$OMT_PATH"
+  mkdir -p "$HOME/.config/tmux"
+  ln -s -f "$OMT_PATH/.tmux.conf" "$HOME/.config/tmux/tmux.conf"
 else
-  echo "~/.tmux already exists. skipping."
+  echo "$OMT_PATH already exists. skipping."
 fi
 
 if [[ ! -d ~/.tmuxifier ]]; then
   git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
 else
-  echo "~/.tmuxifier already exists. skipping."
+  echo "$HOME/.tmuxifier already exists. skipping."
 fi
