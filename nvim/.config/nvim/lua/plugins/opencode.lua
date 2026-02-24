4e894e55a52e93be7dbeefe9caee0cc00d112cc9
Emrecan Altinsoy <emrecan.altinsoy@se.com>
2026-02-24 12:32:57 +0100

    Remove opencode provider since it is removed

--- nvim/.config/nvim/lua/plugins/opencode.lua
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -5,34 +5,34 @@ return {
     config = function()
       vim.g.opencode_opts = {
         port = 33337,
-        provider = {
-          enabled = "tmux",
-          tmux = {
-            options = "-h",
-            focus = false,
-          },
-        },
+        -- provider = {
+        --   enabled = "tmux",
+        --   tmux = {
+        --     options = "-h",
+        --     focus = false,
+        --   },
+        -- },
       }
       -- Required for `opts.events.reload`.
       vim.o.autoread = true
 
       -- Override the stop method to kill the pane instead of just the process
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyDone",
-        callback = function()
-          local provider = require("opencode.config").provider
-          if provider and provider.name == "tmux" then
-            provider.stop = function(self)
-              local pid = self:get_pid()
-              if self.pane_id and pid then
-                require("opencode.provider.util").kill(pid)
-                vim.fn.system("tmux kill-pane -t " .. self.pane_id)
-                self.pane_id = nil
-              end
-            end
-          end
-        end,
-      })
+      -- vim.api.nvim_create_autocmd("User", {
+      --   pattern = "LazyDone",
+      --   callback = function()
+      --     local provider = require("opencode.config").provider
+      --     if provider and provider.name == "tmux" then
+      --       provider.stop = function(self)
+      --         local pid = self:get_pid()
+      --         if self.pane_id and pid then
+      --           require("opencode.provider.util").kill(pid)
+      --           vim.fn.system("tmux kill-pane -t " .. self.pane_id)
+      --           self.pane_id = nil
+      --         end
+      --       end
+      --     end
+      --   end,
+      -- })
 
       vim.keymap.set({ "n", "x" }, "<leader>oa", function()
         require("opencode").ask("@this: ", { submit = true })
