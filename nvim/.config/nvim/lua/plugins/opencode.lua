e394d0a3dc14472a40c603f137923aaa99fa9b82
Emrecan Altinsoy <emrecanaltinsoy@gmail.com>
2026-02-24 17:07:20 +0100

    Open opencode in snacks terminal

--- nvim/.config/nvim/lua/plugins/opencode.lua
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -3,7 +3,31 @@ return {
     "NickvanDyke/opencode.nvim",
     dependencies = { "folke/snacks.nvim" },
     config = function()
+      local opencode_cmd = "opencode --port"
+      ---@type snacks.terminal.Opts
+      local snacks_terminal_opts = {
+        win = {
+          position = "right",
+          enter = false,
+          on_win = function(win)
+            -- Set up keymaps and cleanup for an arbitrary terminal
+            require("opencode.terminal").setup(win.win)
+          end,
+        },
+      }
+      ---@type opencode.Opts
       vim.g.opencode_opts = {
+        server = {
+          start = function()
+            require("snacks.terminal").open(opencode_cmd, snacks_terminal_opts)
+          end,
+          stop = function()
+            require("snacks.terminal").get(opencode_cmd, snacks_terminal_opts):close()
+          end,
+          toggle = function()
+            require("snacks.terminal").toggle(opencode_cmd, snacks_terminal_opts)
+          end,
+        },
         port = 33337,
         -- provider = {
         --   enabled = "tmux",
