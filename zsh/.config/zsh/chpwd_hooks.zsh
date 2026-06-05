b2145394899ef33343eff5e95aa682ca24f9fae8
Emrecan Altinsoy <emrecanaltinsoy@gmail.com>
2026-06-05 12:13:41 +0200

    Organize shared-aliases, robustify load-nvmrc hook with scenario messages

--- zsh/.config/zsh/chpwd_hooks.zsh
+++ zsh/.config/zsh/chpwd_hooks.zsh
@@ -15,13 +15,16 @@ check_and_activate_venv() {
 
 # Auto switch node version using nvm
 load-nvmrc() {
+    if ! command -v nvm >/dev/null 2>&1; then
+        return
+    fi
     local nvmrc_path
     nvmrc_path="$(nvm_find_nvmrc)"
-
     if [ -n "$nvmrc_path" ]; then
         local nvmrc_node_version
-        nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-
+        local nvmrc_config_version
+        nvmrc_config_version=$(cat "${nvmrc_path}")
+        nvmrc_node_version=$(nvm version "$nvmrc_config_version")
         if [ "$nvmrc_node_version" = "N/A" ]; then
             nvm install
         elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
@@ -32,18 +35,18 @@ load-nvmrc() {
     fi
 }
 
+
 # --- Hook Registration ---
 
 autoload -U add-zsh-hook
 add-zsh-hook chpwd check_and_activate_venv
-if [ -x "$(command -v nvm)" ]; then
-    add-zsh-hook chpwd load-nvmrc
-fi
+autoload -U add-zsh-hook
+add-zsh-hook chpwd load-nvmrc
 
 # --- Run both functions once on shell startup ---
 
 check_and_activate_venv
-if [ -x "$(command -v nvm)" ]; then
+if command -v nvm >/dev/null 2>&1; then
     load-nvmrc
 fi
 
