7f80cddd27c6f1d390d2ac3cefcb445b8cf93bea
Emrecan Altinsoy <emrecanaltinsoy@gmail.com>
2026-09-12 14:46:38 +0200

    feat(nvim): udpate markkdown preview config

---
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,54 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function()
+    require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+    vim.fn["mkdp#util#install"]()
+  end,
+  keys = {
+    {
+      "<leader>cp",
+      ft = "markdown",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Markdown Preview",
+    },
+  },
+  config = function()
+    vim.cmd([[do FileType]])
+  end,
+  init = function()
+    vim.g.mkdp_auto_start = 0
+    vim.g.mkdp_auto_close = 0
+    vim.g.mkdp_refresh_slow = 0
+    vim.g.mkdp_command_for_global = 0
+    vim.g.mkdp_open_to_the_world = 0
+    vim.g.mkdp_open_ip = ""
+    vim.g.mkdp_browser = ""
+    vim.g.mkdp_echo_preview_url = 0
+    vim.g.mkdp_browserfunc = ""
+    vim.g.mkdp_preview_options = {
+      mkit = {},
+      katex = {},
+      uml = {},
+      maid = {},
+      disable_sync_scroll = 0,
+      sync_scroll_type = "middle",
+      hide_yaml_meta = 1,
+      sequence_diagrams = {},
+      flowchart_diagrams = {},
+      content_editable = false,
+      disable_filename = 0,
+      toc = {},
+    }
+    vim.g.mkdp_markdown_css = ""
+    vim.g.mkdp_highlight_css = ""
+    vim.g.mkdp_port = ""
+    vim.g.mkdp_page_title = "「${name}」"
+    vim.g.mkdp_images_path = vim.fn.expand("~/.markdown_images")
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_theme = "dark"
+    vim.g.mkdp_combine_preview = 1
+    vim.g.mkdp_combine_preview_auto_refresh = 1
+  end,
+}
